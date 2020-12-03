33e83ec67f38af73f6f6dfdf9ba49c505b18d98f
Karolis šlipaitis <karolis.slipaitis@telesoftas.com>
2020-12-03 19:40:18 +0200

    Solution for Day 3 Part 1

---
+++ src/main/java/_2020/day3/TobogganTrajectory.java
@@ -0,0 +1,62 @@
+package _2020.day3;
+
+import lombok.extern.java.Log;
+import util.SolvableTask;
+
+import java.lang.invoke.MethodHandles;
+import java.util.stream.IntStream;
+
+@Log
+public class TobogganTrajectory implements SolvableTask {
+
+    private static final char TREE_SYMBOL = '#';
+
+    private char[][] dataMatrix;
+    private int matrixWidth;
+    private int matrixHeight;
+
+    public static void main(String[] args) {
+        new TobogganTrajectory().solve();
+    }
+
+    @Override
+    public String getPackageName() {
+        return MethodHandles.lookup().lookupClass().getPackageName();
+    }
+
+    @Override
+    public void solve() {
+        prepareData();
+        int result = findTreesCount(1, 3);
+        log.info(String.valueOf(result));
+    }
+
+    private void prepareData() {
+        String[] inputLines = getInputLines();
+
+        matrixWidth = inputLines[0].length();
+        matrixHeight = inputLines.length;
+        dataMatrix = new char[matrixHeight][matrixWidth];
+
+        IntStream.range(0, matrixHeight)
+                .forEach(index -> dataMatrix[index] = inputLines[index].toCharArray());
+    }
+
+    private int findTreesCount(int heightIncrement, int widthIncrement) {
+        int slopeHeight = 0;
+        int slopeWidth = 0;
+        int treeCount = 0;
+
+        do {
+            if (dataMatrix[slopeHeight % matrixHeight][slopeWidth % matrixWidth] == TREE_SYMBOL)
+                treeCount++;
+
+            slopeHeight += heightIncrement;
+            slopeWidth += widthIncrement;
+
+        } while (slopeHeight < dataMatrix.length);
+
+        return treeCount;
+    }
+
+}
