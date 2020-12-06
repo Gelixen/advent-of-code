c81e69b39f9bcba1d305a0031fd6e21f954f7a41
Karolis šlipaitis <karolis.slipaitis@telesoftas.com>
2020-12-06 17:29:48 +0200

    Extract answersOccurrence collection logic

--- src/main/java/_2020/day6/CustomCustoms.java
+++ src/main/java/_2020/day6/CustomCustoms.java
@@ -12,8 +12,20 @@ import java.util.stream.Collectors;
 @Log
 public class CustomCustoms implements SolvableTask {
 
+    @Override
+    public String getPackageName() {
+        return MethodHandles.lookup().lookupClass().getPackageName();
+    }
+
     public static void main(String[] args) {
         new CustomCustoms().solve();
+    }
+
+    @Override
+    public void solve() {
+        long result = sumGroupsAgreedAnswers(getInputLines("\n\n"));
+
+        log.info(String.valueOf(result));
     }
 
     public static long sumGroupsAgreedAnswers(String[] inputLines) {
@@ -23,32 +35,24 @@ public class CustomCustoms implements SolvableTask {
     }
 
     public static long countGroupAgreedAnswers(String groupAnswers) {
-        Map<Integer, Long> lettersOccurrenceMap = groupAnswers.chars()
-                .filter(CustomCustoms::isNotNewLine)
-                .boxed()
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        Map<Integer, Long> answersOccurrenceMap = collectAnswersOccurrenceMap(groupAnswers);
 
         int peopleInGroupCount = groupAnswers.split("\n").length;
 
-        return lettersOccurrenceMap.values().stream()
-                .filter(letterOccurrence -> peopleInGroupCount == letterOccurrence)
+        return answersOccurrenceMap.values().stream()
+                .filter(answerOccurrence -> peopleInGroupCount == answerOccurrence)
                 .count();
+    }
+
+    private static Map<Integer, Long> collectAnswersOccurrenceMap(String groupAnswers) {
+        return groupAnswers.chars()
+                .filter(CustomCustoms::isNotNewLine)
+                .boxed()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
     private static boolean isNotNewLine(int symbol) {
         return symbol != '\n';
-    }
-
-    @Override
-    public String getPackageName() {
-        return MethodHandles.lookup().lookupClass().getPackageName();
-    }
-
-    @Override
-    public void solve() {
-        long result = sumGroupsAgreedAnswers(getInputLines("\n\n"));
-
-        log.info(String.valueOf(result));
     }
 
 }
