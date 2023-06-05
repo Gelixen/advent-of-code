1b28a11198a38b2c1dd33753508f361e54110ad0
Karolis Šlipaitis <slipaitis.karolis@gmail.com>
2023-06-05 19:23:57 +0300

    add solution for 2023 Day 6 Part 1

---
+++ src/main/java/_2022/day6/IndexWithWindowPair.java
@@ -0,0 +1,4 @@
+package _2022.day6;
+
+public record IndexWithWindowPair(int index, String window) {
+}
---
+++ src/main/java/_2022/day6/TuningTrouble.java
@@ -0,0 +1,31 @@
+package _2022.day6;
+
+import lombok.extern.java.Log;
+import util.SolvableTask;
+
+import java.util.stream.IntStream;
+
+@Log
+public class TuningTrouble implements SolvableTask {
+
+    public static void main(String[] args) {
+        new TuningTrouble().solve();
+    }
+
+    @Override
+    public void solve() {
+        String input = getInputLines()[0];
+
+        int windowSize = 4;
+
+        IndexWithWindowPair indexWithWindowPair = IntStream.range(0, input.length() - windowSize)
+                .mapToObj(index -> new IndexWithWindowPair(index, input.substring(index, index + windowSize)))
+                .filter(pair -> pair.window().chars().distinct().count() == windowSize)
+                .limit(1)
+                .toList()
+                .get(0);
+
+        log.info(String.valueOf(indexWithWindowPair.index() + windowSize));
+    }
+
+}
