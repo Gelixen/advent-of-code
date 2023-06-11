804fa39cf5c99ab2e93cbb16f6df4c0619ddb2ad
Karolis Šlipaitis <slipaitis.karolis+github@gmail.com>
2023-06-11 21:03:35 +0300

    add solution for 2023 Day 13 Part 2

--- src/main/java/_2022/day13/DistressSignal.java
+++ src/main/java/_2022/day13/DistressSignal.java
@@ -1,15 +1,22 @@
 package _2022.day13;
 
 import static java.lang.Boolean.TRUE;
+import static java.util.function.Predicate.not;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.java.Log;
 import util.SolvableTask;
 
 @Log
 public class DistressSignal implements SolvableTask {
+
+    private static final String DIVIDER_PACKET_1 = "[[2]]";
+    private static final String DIVIDER_PACKET_2 = "[[6]]";
 
     public static void main(String[] args) {
         new DistressSignal().solve();
@@ -17,21 +24,35 @@ public class DistressSignal implements SolvableTask {
 
     @Override
     public void solve() {
-        String[] inputLines = getInputLines();
-        int rightOrderPairsSum = 0;
+        List<String> list = Arrays.stream(getInputLines()).collect(Collectors.toList());
+        list.add(DIVIDER_PACKET_1);
+        list.add(DIVIDER_PACKET_2);
 
-        for (int i = 0; i < inputLines.length; i += 3) {
-            String left = inputLines[i];
-            String right = inputLines[i + 1];
+        List<String> sortedLines = list.stream()
+                .filter(not(String::isBlank))
+                .sorted((l1, l2) -> compareIfInRightOrder(l1, l2) ? -1 : 1)
+                .toList();
 
-            if (compareIfInRightOrder(left, right)) {
-                int pairNumber = i / 3 + 1;
-                rightOrderPairsSum += pairNumber;
+        int multiplied = getMultipliedDividerPacketIndexes(sortedLines);
+
+        log.info(String.valueOf(multiplied));
+    }
+
+    private static int getMultipliedDividerPacketIndexes(List<String> lines) {
+        int dividerPacket1Index = 0;
+        int dividerPacket2Index = 0;
+
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            if (line.equals(DIVIDER_PACKET_1)) {
+                dividerPacket1Index = i + 1;
+            }
+            if (line.equals(DIVIDER_PACKET_2)) {
+                dividerPacket2Index = i + 1;
             }
         }
 
-        log.info(String.valueOf(rightOrderPairsSum));
-
+        return dividerPacket1Index * dividerPacket2Index;
     }
 
     boolean compareIfInRightOrder(String left, String right) {
@@ -50,8 +71,12 @@ public class DistressSignal implements SolvableTask {
             int left = leftPacket.getAsInt();
             int right = rightPacket.getAsInt();
 
-            if (left < right) return true;
-            if (left > right) return false;
+            if (left < right) {
+                return true;
+            }
+            if (left > right) {
+                return false;
+            }
             return null;
         }
 
@@ -99,7 +124,7 @@ public class DistressSignal implements SolvableTask {
     private static boolean isBothIntegers(JsonElement leftPacket, JsonElement rightPacket) {
         return leftPacket.isJsonPrimitive() && rightPacket.isJsonPrimitive();
     }
-    
+
     private static boolean isBothArrays(JsonElement leftPacket, JsonElement rightPacket) {
         return leftPacket.isJsonArray() && rightPacket.isJsonArray();
     }
