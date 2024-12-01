bc47f124d5606e9a62da0a34ffd70a0414793fd1
Karolis Šlipaitis <slipaitis.karolis+github@gmail.com>
2024-12-01 13:33:56 +0200

    add solution for 2024 Day 1 Part 2

--- src/main/java/_2024/day1/HistorianHysteria.java
+++ src/main/java/_2024/day1/HistorianHysteria.java
@@ -1,18 +1,17 @@
 package _2024.day1;
 
-import lombok.extern.java.Log;
-import util.SolvableTask;
+import util.SolvableTwoPartsTask;
 
 import java.util.*;
 
-@Log
-public class HistorianHysteria implements SolvableTask {
+public class HistorianHysteria implements SolvableTwoPartsTask {
     public static void main(String[] args) {
-        new HistorianHysteria().solve();
+        new HistorianHysteria().solve1Part();
+        new HistorianHysteria().solve2Part();
     }
 
     @Override
-    public void solve() {
+    public void solve1Part() {
         Queue<Integer> firstLocationIdsList = new PriorityQueue<>();
         Queue<Integer> secondLocationIdsList = new PriorityQueue<>();
 
@@ -27,6 +26,24 @@ public class HistorianHysteria implements SolvableTask {
         for (int i = 0; i < size; i++) {
             result += Math.abs(firstLocationIdsList.remove() - secondLocationIdsList.remove());
         }
+
+        System.out.println(result);
+    }
+
+    @Override
+    public void solve2Part() {
+        Queue<Integer> firstLocationIdsList = new PriorityQueue<>();
+        Map<Integer, Integer> secondLocationIdsList = new HashMap<>();
+
+        Arrays.stream(getInputLines()).forEach(line -> {
+            String[] locationIds = line.split(" {3}");
+            firstLocationIdsList.add(Integer.valueOf(locationIds[0]));
+            secondLocationIdsList.compute(Integer.valueOf(locationIds[1]), (key, value) -> (value == null) ? 1 : value + 1);
+        });
+
+        var result = firstLocationIdsList.stream()
+                .map(locationId -> locationId * secondLocationIdsList.getOrDefault(locationId, 0))
+                .reduce(0, Integer::sum);
 
         System.out.println(result);
     }
